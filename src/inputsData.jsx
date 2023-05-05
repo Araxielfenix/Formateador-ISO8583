a752be149e8b78ab08d0508b3a3f2421973953ea
Araxiel Fenix <araxielfenix@outlook.com>
2023-05-05 12:01:22 -0600

    Se agregó animacion al checkbox

--- src/inputsData.jsx
+++ src/inputsData.jsx
@@ -38,7 +38,7 @@ function inputs() {
 						Copiar
 					</button>
 					<div class="hidden pt-8" id="switch">
-						<label class="relative inline-flex items-center cursor-pointer">
+						<label class="relative inline-flex items-center cursor-pointer group">
 							<input
 								type="checkbox"
 								value=""
@@ -46,7 +46,7 @@ function inputs() {
 								id="switchInput"
 								onchange={formatear}
 							></input>
-							<div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-indigo-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-indigo-600"></div>
+							<div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-indigo-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-indigo-600 group-hover:ring group-hover:ring-indigo-600"></div>
 							<span class="select-none ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
 								Formatear
 							</span>
